829c62c846623dac42caac22cfa65e736a6b38d4
Michael Freeman <michael.d.freeman90@gmail.com>
2020-02-24 00:19:57 -0700

    Open .txt file and iterate over it finding a palindrome

--- palingram_spells.py
+++ palingram_spells.py
@@ -1,9 +1,6 @@
-import sys
-try:
-    with open("dictionary.txt") as my_file:
-        for x in my_file:
-            print(x)
-except IOError as e:
-    print("{}\nError opening {}. Terminating program.".format(e,my_file),
-            file=sys.stderr)
-    sys.exit(1)
+file = open("dictionary.txt", "r") # Load digital dictionary file as a list of words
+lst = [] # Create an empty list to hold palindromes
+for word in file: # Loop through each word in the word list:
+    if(len(word) > 1 and word == word[::-1]): #   If word sliced is == word sliced backward:
+        lst.append(word) # Append word to the palindrome list
+print(lst) # Print palindrome list
