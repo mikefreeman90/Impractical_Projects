0015456ec8cc733a36c098107fcbb7e0c8235274
Michael Freeman <michael.d.freeman90@gmail.com>
2020-02-23 20:52:51 -0700

    Add new line characters

--- silly_name_generator.py
+++ silly_name_generator.py
@@ -1,6 +1,6 @@
-import random
+import random, sys
 
-print("Hello, welcome to the Silly Name Generator!")
+print("Hello, welcome to the Silly Name Generator! \n")
 # Load a list of first names
 first_names = ["Michael", "Alex", "David", "Colton", "Topher", "Jason", "Freddy", "Siri", "Hector", "Achilles", "Apollo"]
 # Load a list of last names
@@ -10,7 +10,7 @@ quit = 0
 while(quit != '1'):
     rand_first = random.choice(first_names) # Choose a first name at random, and assign the name to a variable
     rand_sur = random.choice(last_names) # Choose a surname at random, and assign the name to a variable
-    print(rand_first, rand_sur, "\033[1;31m" ) # Print the names to the screen in order and in red font
+    print(rand_first, rand_sur, "\033[1;31m" "\n" ) # Print the names to the screen in order and in red font
     # Ask the user to quit or play again
     print("Press enter to play again, enter 1 to quit.")
     quit = input()
